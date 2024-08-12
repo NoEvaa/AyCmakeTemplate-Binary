8011a352f2e78916a3a217be06d8360b43f48e85
NoE <3504893952@qq.com>
2024-08-12 22:07:56 +0800

    update

--- include/aybin/ay_multiply.h
+++ include/aybin/ay_multiply.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#if defined(_WIN32) && !defined(__GNUC__)
+    #define AY_BIN_API __declspec(dllexport)
+#else
+    #define AY_BIN_API
+#endif
+
 namespace iin {
-int  ayMultiply(int, int);
-bool ayMultiply(bool, bool);
+AY_BIN_API int  ayMultiply(int, int);
+AY_BIN_API bool ayMultiply(bool, bool);
 }
--- src/ay_multiply.cc
+++ src/ay_multiply.cc
@@ -2,11 +2,11 @@
 
 int iin::ayMultiply(int l, int r)
 {
-  return l * r;
+    return l * r;
 }
 
 bool iin::ayMultiply(bool l, bool r)
 {
-  return l ? r : false;
+    return l ? r : false;
 }
 
